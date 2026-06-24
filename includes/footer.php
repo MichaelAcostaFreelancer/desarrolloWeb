22e158a8abed1b152c10a3403c615d4c423dcd9e
MichaelAcostaFreelancer <michaelacostafreelancer@gmail.com>
2026-06-24 18:31:08 -0400

    Agregando nuevos archivos al proyecto

---
+++ includes/footer.php
@@ -0,0 +1,4 @@
+
+<footer class="footer">
+        <p>Todos los derechos reservados.<?php echo date('Y'); ?> &copy; </p>
+    </footer>
---
+++ includes/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina Michael</title>
+    <link rel="preload" href="css/normalize.css" as="style">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link href="https://fonts.googleapis.com/css2?family=Krub:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="preload" href="css/styles.css" as="style">
+    <link href="css/styles.css" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <a class=logo href="index.php">
+            <h1> Desarrollo<span> web</span></h1>
+        </a>
+    </header>
+
+    <div class="nav-bg">
+        <nav class="navegacion-principal contenedor">
+            <a href="/index.php">Inicio</a>
+            <a href="/sobre_mi.php">Sobre Mi</a>
+            <a href="/clientes.php">Clientes</a>
+            <a href="/contacto.php">Contacto</a>
+        </nav>
+    </div>
